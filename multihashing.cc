3d7c85baa0b549f2eb382b7a5e2cc32bcf456cf6
MoneroOcean <support@moneroocean.stream>
2020-03-05 13:03:54 -0800

    Bug fix

--- multihashing.cc
+++ multihashing.cc
@@ -70,7 +70,7 @@ struct InitCtx {
 void init_rx(const uint8_t* seed_hash_data, xmrig::Algorithm::Id algo) {
     bool update_cache = false;
     if (!rx_cache[algo]) {
-        VirtualMemory* const m_memory = new VirtualMemory(RANDOMX_CACHE_MAX_SIZE, true, false, 0, 4096);
+        xmrig::VirtualMemory* const m_memory = new VirtualMemory(RANDOMX_CACHE_MAX_SIZE, true, false, 0, 4096);
         rx_cache[algo] = randomx_create_cache(static_cast<randomx_flags>(RANDOMX_FLAG_JIT | RANDOMX_FLAG_LARGE_PAGES), memory->raw());
         if (!rx_cache[algo]) {
             rx_cache[algo] = randomx_create_cache(RANDOMX_FLAG_JIT, memory->raw());
