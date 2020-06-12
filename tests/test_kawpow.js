33f73aacfa7587e2012833214f497941f13e6d47
MoneroOcean <support@moneroocean.stream>
2020-06-12 12:41:58 -0700

    Bug fixes

--- tests/test_kawpow.js
+++ tests/test_kawpow.js
@@ -2,8 +2,8 @@
 const multiHashing = require('../build/Release/cryptonight-hashing');
 
 const result = multiHashing.kawpow(
-	30000, Buffer.from('ffeeddccbbaa9988776655443322110000112233445566778899aabbccddeeff', 'hex'),
-	0x123456789abcdef0, 0x69da467194bac071
+	30000, Buffer.from('ffeeddccbbaa9988776655443322110000112233445566778899aabbccddeefff0debc9a78563412', 'hex'),
+	Buffer.from('0xffffffffffffffff', 'hex')
 );
 
 if (result !== null && result.toString('hex') === '177b565752a375501e11b6d9d3679c2df6197b2cab3a1ba2d6b10b8c71a3d459')
