6d297cbbd6e2da6b3c45da91c0e325574f1c5c92
MoneroOcean <support@moneroocean.stream>
2020-11-01 20:47:00 +0000

    Updated Ravencoin to use faster verify

--- tests/test_perf_kawpow.js
+++ tests/test_perf_kawpow.js
@@ -1,7 +1,7 @@
 "use strict";
 let multiHashing = require('../build/Release/cryptonight-hashing');
 
-const ITER = 100;
+const ITER = 1000000000;
 let input1 = Buffer.from('63543d3913fe56e6720c5e61e8d208d05582875822628f483279a3e8d9c9a8b3', 'hex');
 let input2 = Buffer.from('9b95eb33003ba288', 'hex');
 let input3 = Buffer.from('89732e5ff8711c32558a308fc4b8ee77416038a70995670e3eb84cbdead2e337', 'hex');
