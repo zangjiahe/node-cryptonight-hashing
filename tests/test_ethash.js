2a571b9c419e21a9a50c606cc80750337665d976
MoneroOcean <support@moneroocean.stream>
2021-01-15 03:13:13 +0000

    Ethash support

--- tests/test_ethash.js
+++ tests/test_ethash.js
@@ -2,7 +2,7 @@
 const multiHashing = require('../build/Release/cryptonight-hashing');
 
 const result = multiHashing.ethash(
-	Buffer.from('e99e022112df268087ea7eafaf4790497fd21dbeeb6bd7a1721df161a6657a54', 'hex'),
+	Buffer.from('dc0818cf78f21a8e70579cb46a43643f78291264dda342ae31049421c82d21ae', 'hex'),
 	Buffer.from('689056015818adbe', 'hex'),
 	Buffer.from('4fffe9ae21f1c9e15207b1f472d5bbdd68c9595d461666602f2be20daf5e7843', 'hex')
 );
