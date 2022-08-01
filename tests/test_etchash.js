5a18766a130318cf09804c7a9515a78bc3e4ca13
MoneroOcean <support@moneroocean.stream>
2022-08-01 06:11:51 +0000

    Add ETC support

---
+++ tests/test_etchash.js
@@ -0,0 +1,24 @@
+"use strict";
+const multiHashing = require('../build/Release/cryptonight-hashing');
+
+const result = multiHashing.etchash(
+	Buffer.from('f5afa3074287b2b33e975468ae613e023e478112530bc19d4187693c13943445', 'hex'),
+	Buffer.from('ff4136b6b6a244ec', 'hex'),
+	1257006
+);
+
+if (result !== null && result[0].toString('hex') === '0000000000095d18875acd4a2c2a5ff476c9acf283b4975d7af8d6c33d119c74')
+	console.log('Etchash test result passed');
+else {
+	console.log('Etchash test result failed: ' + (result[0] ? result[0].toString('hex') : result[0]));
+        process.exit(1);
+}
+
+if (result !== null && result[1].toString('hex') === '47da5e47804594550791c24331163c1f1fde5bc622170e83515843b2b13dbe14')
+	console.log('Etchash test mix hash passed');
+else {
+	console.log('Etchash test mix hash failed: ' + (result[1] ? result[1].toString('hex') : result[1]));
+        process.exit(1);
+}
+
+
