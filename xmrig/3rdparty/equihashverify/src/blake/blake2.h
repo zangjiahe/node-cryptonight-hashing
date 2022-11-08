bd7c506e1f4d3e07b0302aee6710ddbdb15d1cd3
yunhai.he <2046095737@qq.com>
2022-11-08 15:23:27 +0800

    "26"

--- xmrig/3rdparty/equihashverify/src/blake/blake2.h
+++ xmrig/3rdparty/equihashverify/src/blake/blake2.h
@@ -36,14 +36,14 @@ extern "C" {
     BLAKE2S_PERSONALBYTES = 8
   };
 
-  enum blake2b_constant
-  {
-    BLAKE2B_BLOCKBYTES = 128,
-    BLAKE2B_OUTBYTES   = 64,
-    BLAKE2B_KEYBYTES   = 64,
-    BLAKE2B_SALTBYTES  = 16,
-    BLAKE2B_PERSONALBYTES = 16
-  };
+//  enum blake2b_constant
+//  {
+//    BLAKE2B_BLOCKBYTES = 128,
+//    BLAKE2B_OUTBYTES   = 64,
+//    BLAKE2B_KEYBYTES   = 64,
+//    BLAKE2B_SALTBYTES  = 16,
+//    BLAKE2B_PERSONALBYTES = 16
+//  };
 
 #pragma pack(push, 1)
   typedef struct __blake2s_param
@@ -71,20 +71,20 @@ extern "C" {
     uint8_t  last_node;
   } blake2s_state;
 
-  typedef struct __blake2b_param
-  {
-    uint8_t  digest_length; // 1
-    uint8_t  key_length;    // 2
-    uint8_t  fanout;        // 3
-    uint8_t  depth;         // 4
-    uint32_t leaf_length;   // 8
-    uint64_t node_offset;   // 16
-    uint8_t  node_depth;    // 17
-    uint8_t  inner_length;  // 18
-    uint8_t  reserved[14];  // 32
-    uint8_t  salt[BLAKE2B_SALTBYTES]; // 48
-    uint8_t  personal[BLAKE2B_PERSONALBYTES];  // 64
-  } blake2b_param;
+//  typedef struct __blake2b_param
+//  {
+//    uint8_t  digest_length; // 1
+//    uint8_t  key_length;    // 2
+//    uint8_t  fanout;        // 3
+//    uint8_t  depth;         // 4
+//    uint32_t leaf_length;   // 8
+//    uint64_t node_offset;   // 16
+//    uint8_t  node_depth;    // 17
+//    uint8_t  inner_length;  // 18
+//    uint8_t  reserved[14];  // 32
+//    uint8_t  salt[BLAKE2B_SALTBYTES]; // 48
+//    uint8_t  personal[BLAKE2B_PERSONALBYTES];  // 64
+//  } blake2b_param;
 
   ALIGN( 64 ) typedef struct __blake2b_state
   {
@@ -121,7 +121,7 @@ extern "C" {
 
   int blake2b_init( blake2b_state *S, const uint8_t outlen );
   int blake2b_init_key( blake2b_state *S, const uint8_t outlen, const void *key, const uint8_t keylen );
-  int blake2b_init_param( blake2b_state *S, const blake2b_param *P );
+//  int blake2b_init_param( blake2b_state *S, const blake2b_param *P );
   int blake2b_update( blake2b_state *S, const uint8_t *in, uint64_t inlen );
   int blake2b_final( blake2b_state *S, uint8_t *out, uint8_t outlen );
 
